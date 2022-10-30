49bea4070b979eda37eff9d6435230537722d64f
JanaKoduje <jjanule@gmail.com>
2022-10-30 22:33:41 +0100

    filtrovani jen nesplnenych

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const tasksApi = fetch('https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks');
+const apiUri = "https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks";
 
-export const Task = ({name, due, done}) => {
+export const Task = ({ name, due, done }) => {
+  const doneMark = done ? "✓" : "";
 
-    const doneMark = done ? "✓" : "";
-
-    return `
+  return `
         <div class="task">
         <div class="task__body">
           <div class="task__name">${name}</div>
@@ -12,39 +11,21 @@ export const Task = ({name, due, done}) => {
         </div>
         <div class="task__done">${doneMark}</div>
       </div>
-      `
-}
-
+      `;
+};
 
 const renderTask = (data) => {
-    const taskListElm = document.querySelector('#taskList')
-    taskListElm.innerHTML = 
-        [
-          data.map( (task) => Task(task))
-        ].join('\n')
-    
-}
+  const taskListElm = document.querySelector("#taskList");
+  taskListElm.innerHTML = [data.map((task) => Task(task))].join("\n");
+};
 
-tasksApi
-    .then( (response) => response.json())
-    .then(renderTask)
+fetch(apiUri)
+  .then((response) => response.json())
+  .then(renderTask);
 
-
-
-const suntime = (data) => {
-    const suntimeElm = document.querySelector('#suntime');
-    suntimeElm.innerHTML = `
-        <p>
-        Východ: ${data.results.sunrise}<br />
-        Západ: ${data.results.sunset}
-        </p>   
-        `        
-}
-
-
-const btnElm = document.querySelector('button');
-btnElm.addEventListener('click', 
-        () => pocasi
-              .then( (response) => response.json())
-              .then(suntime)
-              );
+const checkElm = document.querySelector("#checkbox-undone");
+checkElm.addEventListener("change", (e) => {
+  fetch(e.target.checked ? apiUri + "?done=false" : apiUri)
+    .then((response) => response.json())
+    .then(renderTask);
+});
